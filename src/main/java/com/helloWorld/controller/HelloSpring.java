bec193be0ef0e3c7dbb2c1f48946c598a0ec06e4
MohamedElsayed930 <m.elsaied.930@gmail.com>
2017-05-01 11:38:40 -0400

    First Successful Commit

--- src/main/java/com/helloWorld/controller/HelloSpring.java
+++ src/main/java/com/helloWorld/controller/HelloSpring.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloSpring {
+	/**
+	 * Main Controller
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = "/hello")
 	public String helloMan(Model model) {
 		model.addAttribute("message", "Hello Mr.Mohamed");
